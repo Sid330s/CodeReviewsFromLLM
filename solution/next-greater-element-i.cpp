cb3bb60ad778217ef80efa9bac17c9e196a21706
Siddharth Kale <52232241+Sid330s@users.noreply.github.com>
2025-04-29 19:51:05 +0000

    chore: status updated

--- solution/next-greater-element-i.cpp
+++ solution/next-greater-element-i.cpp
@@ -1,1 +1,35 @@
-// Solution for next-greater-element-i
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums2.size();
+        stack<int> st;
+        unordered_map<int,int> nextGreater;
+
+        int maxIdx = n-1;
+        nextGreater[nums2[maxIdx]] = -1;
+
+        st.push(nums2[maxIdx]);
+
+        for (int k = 1; k <= n-1; k++) {
+            int i = (maxIdx - k + n) % n;
+            while (!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
+            }
+            if (!st.empty()) {
+                nextGreater[nums2[i]]=st.top();
+            }
+            else{
+                nextGreater[nums2[i]]=-1;
+            }
+            st.push(nums2[i]);
+        }
+
+        for(int i=0;i<nums1.size();i++){
+            nums1[i] = nextGreater[nums1[i]];
+        }
+
+
+        return nums1;
+
+    }
+};
